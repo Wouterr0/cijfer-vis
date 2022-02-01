f423f6f40251448dbc8c0d85a3e11b40e5b18e46
Wouterr0 <ruiterwouter@gmail.com>
2022-02-01 10:52:41 +0100

    Update data to allign with the current PTA

--- data.js
+++ data.js
@@ -5,26 +5,12 @@ schema = [
         shortname: "Na.",
         assignments: [
             {
-                type: "COMB",
-                weight: 1,
-                assignments: [
-                    {
-                        type: "PO",
-                        year: 5,
-                        period: 1,
-                        description: "Verslaglegging (individueel of in tweetallen) van een experiment rondom een periodiek verschijnsel",
-                        domains: ["A", "I1"],
-                        weight: .5
-                    },
-                    {
-                        type: "MET",
-                        year: 5,
-                        period: 1,
-                        description: "Engelstalige presentatie in groepsverband over een aan klimaatverandering en duurzaamheid gerelateerd mondiaal probleem",
-                        domains: ["A", "G2"],
-                        weight: .5
-                    }
-                ]
+                type: "PO",
+                year: 5,
+                period: 1,
+                description: "Engelstalige presentatie in groepsverband over een aan klimaatverandering en duurzaamheid gerelateerd mondiaal probleem",
+                domains: ["A", "G2"],
+                weight: 16
             },
             {
                 type: "SET",
@@ -32,29 +18,31 @@ schema = [
                 period: 2,
                 description: "Toets over periodieke verschijnselen, biofysica, materiaaleigenschappen en medische beeldvorming",
                 domains: ["A", "B1", "B2", "E1", "G1"],
-                weight: 1
+                weight: 34
             },
             {
-                type: "COMB",
-                weight: 1,
-                assignments: [
-                    {
-                        type: "PO",
-                        year: 5,
-                        period: 3,
-                        description: "Technisch ontwerp van een robot opstellen in groepsverband",
-                        domains: ["A", "D1", "I3"],
-                        weight: .5
-                    },
-                    {
-                        type: "PO",
-                        year: 5,
-                        period: 3,
-                        description: "Modelstudie over een bewegingsverschijnsel Individueel werk",
-                        domains: ["A", "I2"],
-                        weight: .5
-                    }
-                ]
+                type: "PO",
+                year: 5,
+                period: 3,
+                description: "Technisch ontwerp van een robot opstellen in groepsverband",
+                domains: ["A", "I3"],
+                weight: 16
+            },
+            {
+                type: "PO",
+                year: 5,
+                period: 3,
+                description: "Individueel portfolio van verslaglegging van experimenten op het gebied van periodieke verschijnselen en elektromagnetisme.",
+                domains: ["A", "I1"],
+                weight: 17
+            },
+            {
+                type: "PO",
+                year: 5,
+                period: 4,
+                description: "Modelstudie over een bewegingsverschijnsel\nIndividueel werk",
+                domains: ["A", "I2"],
+                weight: 17
             }
         ]
     },
@@ -64,44 +52,44 @@ schema = [
         shortname: "Sck.",
         assignments: [
             {
-                type: "MET",
+                type: "PO",
                 year: 5,
                 period: 1,
                 description: "Engelstalige presentatie in groepsverband over een door klimaatverandering veroorzaakt mondiaal probleem",
-                domains: ["A", "E3", "F5", "G4", "G5"],
-                weight: 1
+                domains: ["A", "C9", "E3", "F4", "G4"],
+                weight: 15
             },
             {
                 type: "SET",
                 year: 5,
                 period: 2,
-                description: "Toets over de werking van het menselijk lichaam op gebied van voeding en gezondheid",
-                domains: ["A", "C7", "D4"],
-                weight: 1
+                description: "Schriftelijk examen over de chemie van voeding en gezondheid",
+                domains: ["A", "B1", "C1", "C2", "C5", "C7"],
+                weight: 30
             },
             {
                 type: "PO",
                 year: 5,
                 period: 3,
+                description: "Individuele opdracht naar aanleiding van een bezoek aan externe organisatie op het gebied van duurzaamheid",
+                domains: ["A", "C8", "F5", "G5"],
+                weight: 20
+            },
+            {
+                type: "PO",
+                year: 5,
+                period: 4,
                 description: "Individuele verslaglegging van uitvoer van en werkhouding voor verscheidene practica op het gebied van gezondheid",
-                domains: ["A", "C5", "C8", "D2", "F4"],
-                weight: 1
-            },
-            {
-                type: "PO",
-                year: 5,
-                period: 3,
-                description: "Individueel verslag van een bezoek aan externe organisatie op het gebied van duurzaamheid",
-                domains: ["A", "C9", "C10"],
-                weight: 1
+                domains: ["A", "C10", "D2", "D4", "C5"],
+                weight: 20
             },
             {
                 type: "PO",
                 year: 6,
-                period: 1,
-                description: "Werkplan opstellen voor gekozen onderzoeksvraag die betrekking hebben op bijvoorbeeld nanotechnologie. Eventueel uit te voeren.",
+                period: 2,
+                description: "Onderzoeks- of ontwerpopdracht formuleren over een nieuwe chemische toepassing in de bestaande markt, deze uitvoeren en daar een verslag van doen.",
                 domains: ["A", "E4", "E5"],
-                weight: 1
+                weight: 15
             }
         ]
     },
@@ -121,7 +109,7 @@ schema = [
                         period: 1,
                         description: "Politieke partijen en opvattingen over de verzorgingsstaat boekje in groepjes",
                         domains: ["A", "C", "D"],
-                        weight: 1
+                        weight: 25
                     },
                     {
                         type: "PO",
@@ -129,7 +117,7 @@ schema = [
                         period: 2,
                         description: "Rechten, rechtvaardigheid en de pluriforme samenleving documentaire in groepjes",
                         domains: ["A", "B", "C", "E"],
-                        weight: 1
+                        weight: 25
                     },
                     {
                         type: "SET",
@@ -137,7 +125,7 @@ schema = [
                         period: 1,
                         description: "Politiek: regels en instituties",
                         domains: ["B", "C", "D"],
-                        weight: 1
+                        weight: 25
                     },
                     {
                         type: "PO",
@@ -145,7 +133,7 @@ schema = [
                         period: 2,
                         description: "Portfolio: maatschappelijke instituties en ontwikkelingen",
                         domains: ["A", "B", "C", "D", "E"],
-                        weight: 1
+                        weight: 25
                     }
                 ],
                 weight: 1
@@ -159,33 +147,41 @@ schema = [
                         type: "PO",
                         year: 5,
                         period: 1,
-                        description: "Duo's\n- Technisch ontwerp (ontwerpcyclus)\n- Product of prototype\n- Reflectie op duurzaamheidsfilosofie",
+                        description: "Duurzaamheid\nDuo's\n- Technisch ontwerp (ontwerpcyclus)\n- Product of prototype\n- Reflectie op duurzaamheidsfilosofie",
                         domains: ["A2", "A4", "A5", "D2"],
-                        weight: 1,
+                        weight: 20
+                    },
+                    {
+                        type: "SET",
+                        year: 5,
+                        period: 2,
+                        description: "Wetenschapsfilosofie\n- Reductionisme\n- Correspondentietheorie\n- Kritisch rationalisme\n- Coherentietheorie\n- Coventionalisme\n- Pragmatisme",
+                        domains: ["B1", "B2", "C3", "F3"],
+                        weight: 20
                     },
                     {
                         type: "PO",
                         year: 5,
                         period: 3,
-                        description: "Duo's\nModelleren (Game Theory)\nModel maken\n\nVerslag schrijven\n- Wetenschapsfilosofie\n- Beschouwing Game\nTheory\n- Reflectie Modelleren",
+                        description: "Modelleren\nDuo's\n\nProgrammeren en toelichten van een model in Netlogo",
                         domains: ["A6", "B1", "B2", "C3", "F3"],
-                        weight: 1,
+                        weight: 20
                     },
                     {
                         type: "PO",
                         year: 5,
                         period: 4,
-                        description: "Duo of individueel\nData Science toegepast in profielwerkstuk",
-                        domains: ["A1", "A3", "B1", "B2"],
-                        weight: 1,
+                        description: "Onderzoekscyclus voor PWS\nDuo of individueel\nData Science toegepast in profielwerkstuk",
+                        domains: ["A1", "A3"],
+                        weight: 20
                     },
                     {
                         type: "MET",
                         year: 6,
                         period: 1,
-                        description: "Debat (2 tegen 2) individuele beoordeling\n\nOnderwerp:\nCultuurkritiek",
+                        description: "Cultuurkritiek\nDebat (2 tegen 2) over de invloed van technologie op het individu en de maatschappij\nIndividuele beoordeling",
                         domains: ["A7", "B3"],
-                        weight: 1,
+                        weight: 20,
                     }
                 ],
                 weight: 1
@@ -201,7 +197,7 @@ schema = [
                         period: 1,
                         description: "Corporate identity boek politieke partijen op basis van politieke ideologie en geschiedenis in groepjes",
                         domains: ["A", "B", "C", "D"],
-                        weight: 1,
+                        weight: 33,
                     },
                     {
                         type: "PO",
@@ -209,15 +205,15 @@ schema = [
                         period: 2,
                         description: "Documentaire maken over maatschappelijk onderwerp in groepjes",
                         domains: ["A", "B", "C", "D"],
-                        weight: 1,
+                        weight: 33,
                     },
                     {
                         type: "PO",
                         year: 5,
-                        period: 1,
+                        period: 2,
                         description: "Portfolio CKV-activiteiten: keuze, bijwonen, beschrijving en analyse van 4 verschillende culturele activiteiten. Presentatie in een portfolio. (individueel)",
                         domains: ["A", "B", "C", "D"],
-                        weight: 1,
+                        weight: 34,
                     }
                 ],
                 weight: 1
