5bf4f5e46ac8fa007eb8c99fd61aae5491f32862
Wouterr0 <ruiterwouter@gmail.com>
2022-04-04 19:17:39 +0200

    add magister for assignment

--- src/data.js
+++ src/data.js
@@ -548,6 +548,7 @@ export default {
                     description: 'Luistertoets Engels Cito',
                     domains: ['B'],
                     weight: 30,
+                    magister: 'ENTL8302',
                     id: 'e484pmwj16',
                 },
             ],
