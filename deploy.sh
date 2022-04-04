a8e1976f15be5aee7e7d2dda0f2bd0afd9d98b3d
Wouterr0 <ruiterwouter@gmail.com>
2022-04-04 17:23:11 +0200

    add deploy again

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,13 @@
 #!/usr/bin/env sh
 
-# abort on errors
-set -e
+set -xe
 
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+# add dist directory
+git add dist -f
 
-# if you are deploying to a custom domain
-echo 'pta.wouterr.com' > CNAME
+git commit -m "deploy"
 
-git init
-git checkout -b main
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-cd -
+git subtree push --prefix dist origin gh-pages
