e4eb212e31f2a710b1c08febb85e8b92ec1aa9b7
Wouterr0 <ruiterwouter@gmail.com>
2022-04-07 20:25:12 +0200

    github pages only understands the docs/ folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
 const root = resolve(__dirname, 'src');
-const outDir = resolve(__dirname, 'dist');
+const outDir = resolve(__dirname, 'docs');
 
 // https://vitejs.dev/config/
 export default defineConfig({
