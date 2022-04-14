2e3b8a34250efc0f7b52a028aaff0bd6810a5781
Wouterr0 <ruiterwouter@gmail.com>
2022-04-14 20:45:27 +0200

    build instead of deploy

---
+++ build.sh
@@ -0,0 +1,2 @@
+npm run build
+echo "pta.wouterr.com" > docs/CNAME
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-set -ex
-
-git switch main
-npm run build
-echo "pta.wouterr.com" > docs/CNAME
-git commit -am "deploy"
-git push origin main
